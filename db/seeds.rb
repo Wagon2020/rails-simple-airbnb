8c352011104eff0346a23536a154faec22f3adf9
Stefan Sarow <stefan@pixelaffairs.com>
2020-05-20 15:26:01 +0200

    model and controller for flat setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating some flats"
+
+Flat.delete_all
+
+10.times do
+  name = Faker::TvShows::Simpsons.location
+  address = Faker::Address.full_address
+  description = Faker::Hipster.sentence
+  price_per_night = rand(29..1000)
+  number_of_guests = rand(1..10)
+  flat = Flat.new(name: name, address: address, description: description, price_per_night: price_per_night, number_of_guests: number_of_guests)
+  puts "#{flat.name} created"
+  flat.save!
+end
+
+
